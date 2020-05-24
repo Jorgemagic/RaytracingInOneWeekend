f2bd5dee66be168121a723086c4f5e7c9abcc710
jorgemagic <jorgemagic@gmail.com>
2020-05-24 02:23:58 +0200

    - Removed dirty project

--- RaytracingWeekend/Materials/Material.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Numerics;
-
-namespace RaytracingWeekend
-{
-    public interface Material
-    {
-        public bool Scatter(Ray r_in, Hit_Record rec, out Vector3 attenuation, out Ray scattered);
-    }
-}
